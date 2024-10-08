421b790fb7299c097d292d06b2b296ced4d97d6c
Ucokkks <glenmartua@gmail.com>
2024-10-08 09:57:08 +0700

    main

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Main() {
+
+  return (
+    <html>
+      <body>
+        <p>kontol</p>
+      </body>
+    </html>
+
+    
+  )
+}
+
+
+
+export default Main
